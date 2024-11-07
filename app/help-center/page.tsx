60c1a771a13c3f3f5c08f3babb096124785224c2
Aike <aikenyanlynnoo.dev@gmail.com>
2024-11-07 10:29:51 +0700

    add help center page

---
+++ app/help-center/page.tsx
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+
+import { ContactUsSection } from "@/components/molecules/ContactUsSection";
+import { getHeaderFooter } from "@/utils/apiFunctions";
+
+export default async function ContactUsPage() {
+  return <>Help center</>;
+}
