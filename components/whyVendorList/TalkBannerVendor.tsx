4686b08efe955bffacd1cbc710f6cd3d25ee2860
Yoon Myat Sandi <yoonmyatsandi6@gmail.com>
2024-11-13 09:52:26 +0630

    fix banner height

--- components/whyVendorList/TalkBannerVendor.tsx
+++ components/whyVendorList/TalkBannerVendor.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const TalkBannerVendor = () => {
   return (
     <div className="relative w-full h-auto md:h-[250px] lg:h-[200px] mt-10">
-      <div className="absolute bottom-0 w-full h-1/2 md:h-1/2 translate-y-1/2 bg-gradient-to-r from-[#211A4C] to-[#323081] z-0" />
+      <div className="absolute bottom-[-10px] w-full h-3/4 md:h-[70%] translate-y-2/3 bg-gradient-to-r from-[#211A4C] to-[#323081] z-0" />
       <div className="absolute inset-0 w-full h-full px-4 md:px-6 lg:px-12 xl:px-20">
         <div className="relative w-full h-full">
           <Image
@@ -28,7 +28,7 @@ const TalkBannerVendor = () => {
             </p>
           </div>
           <div className="mt-4 md:mt-0 md:ml-8 flex justify-center md:justify-start">
-            <button className="bg-white text-black px-6 py-3 rounded-md shadow-lg hover:bg-primary-700 transition w-full md:w-auto sm:bg-white sm:text-black sm:hover:bg-primary-600 md:bg-primary-600 md:text-white">
+            <button className="bg-white text-black font-semibold px-6 py-3 rounded-md shadow-lg hover:bg-primary-700 transition w-full md:w-auto sm:bg-white sm:text-black sm:hover:bg-primary-600 md:bg-primary-600 md:text-white">
               Talk To Our Partnership Specialist
             </button>
           </div>
