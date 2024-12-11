4b5a0e9c3155432a5cf0e7acaca3b39929ad977a
Yoon Myat Sandi <yoonmyatsandi6@gmail.com>
2024-12-11 11:14:04 +0630

    updated pvm(banner+our latest event sec+res view)

--- components/pvm/PvmEventsCard.tsx
+++ components/pvm/PvmEventsCard.tsx
@@ -20,20 +20,20 @@ export const PvmEventsCard = ({
     <div className="mt-8 relative rounded-lg overflow-hidden shadow-lg w-full max-w-[539px] h-[320px]">
       <img src={imageUrl} alt={imgAlt} className="w-full h-full object-cover" />
 
-      <div className="absolute top-6 left-4 sm:hidden z-20">
+      <div className="absolute top-6 left-4 md:hidden z-20">
         <img src={logoUrl} alt={logoAlt} className="h-7" />
       </div>
 
       <div
         className="
         absolute top-0 left-0 w-full 
-        h-[50%] sm:h-1/3 md:h-38 
+        h-[50%] md:h-1/3 md:h-38 
         backdrop-blur-md bg-gray-300 bg-opacity-30 
         flex flex-col text-left 
         p-6 md:p-4 
-        z-10 sm:z-20"
+        z-10 md:z-20"
       >
-        <h3 className="text-white text-2xl sm:text-3xl font-bold mt-12 sm:mt-0">
+        <h3 className="text-white text-2xl sm:text-3xl font-bold mt-12 md:mt-0">
           {eventTitle}
         </h3>
         <p className="text-white text-sm sm:text-base mt-3">{eventDate}</p>
