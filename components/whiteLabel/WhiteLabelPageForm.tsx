3f02a7ea4ec2f14a796f31f4c9df8bc54218132f
Yoon Myat Sandi <yoonmyatsandi6@gmail.com>
2024-12-18 12:44:46 +0630

    update contact and schedule modal for mobile,tablet, schedule CaretDown in the modal, sticky effect at the bottom, fix ph margin top

--- components/whiteLabel/WhiteLabelPageForm.tsx
+++ components/whiteLabel/WhiteLabelPageForm.tsx
@@ -177,7 +177,7 @@ const WhiteLabelPageForm = ({
               </div>
 
               {/* Phone Input */}
-              <div className="flex-1 mt-4 md:mt-0">
+              <div className="flex-1 md:mt-[-3px]">
                 <span className="text-secondary-700 block mb-2 text-sm font-medium leading-5">
                   Phone
                 </span>
