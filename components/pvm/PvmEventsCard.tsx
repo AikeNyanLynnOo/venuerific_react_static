fa44d2de0935238f27005aefbd342dc906cd3d08
Yoon Myat Sandi <yoonmyatsandi6@gmail.com>
2024-12-20 12:58:07 +0630

    update Our latest event sec's card size, logo size and fontsize

--- components/pvm/PvmEventsCard.tsx
+++ components/pvm/PvmEventsCard.tsx
@@ -17,45 +17,49 @@ export const PvmEventsCard = ({
   eventDate,
 }: PvmEventsCardProps) => {
   return (
-    <div className="mt-8 relative rounded-lg overflow-hidden shadow-lg w-full max-w-[539px] h-[320px]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg min-w-full sm:min-w-[400px] w-fit max-w-[420px] h-[280px] sm:h-[250px]">
       <img src={imageUrl} alt={imgAlt} className="w-full h-full object-cover" />
 
-      <div className="absolute top-6 left-4 md:hidden z-20">
+      <div className="absolute top-6 left-4 md:hidden z-20 ">
         <img src={logoUrl} alt={logoAlt} className="h-7" />
       </div>
 
       <div
         className="
         absolute top-0 left-0 w-full 
-        h-[50%] md:h-1/3 md:h-38 
+        h-[calc(50%+4px)] md:h-[calc(33%+7px)] sm:h-[calc(50%+8px)] 
         backdrop-blur-md bg-gray-300 bg-opacity-30 
         flex flex-col text-left 
-        p-6 md:p-4 
+        p-6 md:pt-4 md:pb-4 md:pl-6 
+        sm:pt-5 sm:pb-5 sm:pl-6 sm:pr-6 
         z-10 md:z-20"
       >
-        <h3 className="text-white text-2xl sm:text-3xl font-bold mt-12 md:mt-0">
+        <h3 className="text-white text-2xl md:text-3xl font-bold mt-11 md:mt-0">
           {eventTitle}
         </h3>
-        <p className="text-white text-sm sm:text-base mt-3">{eventDate}</p>
+        <p className="text-white text-sm md:text-base">{eventDate}</p>
       </div>
 
       <div className="absolute bottom-4 left-4 right-4 p-4 bg-opacity-70 rounded-lg hidden md:flex items-center justify-between">
-        <img src={logoUrl} alt={logoAlt} className="h-10" />
-        <Button
-          className="bg-primary-600 text-white py-2 px-4 rounded-lg shadow-md text-[16px]"
-          variant="solid"
-        >
-          View Details
-        </Button>
+        <img src={logoUrl} alt={logoAlt} className="h-7" />
+        <a href="/" rel="noopener noreferrer" target="_blank">
+          <Button className="bg-primary-600 text-white py-2 px-4 rounded-lg shadow-md font-semibold">
+            View Details
+          </Button>
+        </a>
       </div>
 
       <div className="absolute bottom-4 left-4 right-4 md:hidden">
-        <Button
-          className="w-full bg-primary-600 text-white py-2 px-4 rounded-lg shadow-md text-[16px]"
-          variant="solid"
+        <a
+          href="/"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="absolute bottom-4 left-4 right-4 md:hidden"
         >
-          View Details
-        </Button>
+          <button className="w-full bg-primary-600 text-white py-2 px-4 rounded-lg shadow-md font-semibold">
+            View Details
+          </button>
+        </a>
       </div>
     </div>
   );
