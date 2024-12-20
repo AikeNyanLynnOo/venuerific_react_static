5e28bfd42c7df4e28504efb3e98437ee02e56451
Yoon Myat Sandi <yoonmyatsandi6@gmail.com>
2024-12-20 13:13:04 +0630

    update Our latest event sec's card size, logo size and fontsize, update alignment of title and text in PvmLatestEvent Sec

--- components/pvm/PvmLatestEvent.tsx
+++ components/pvm/PvmLatestEvent.tsx
@@ -17,14 +17,16 @@ const PvmLatestEvent = () => {
   return (
     <div className="w-full mx-auto px-0 md:px-5 lg:px-12 xl:px-20 py-10 bg-primary-50">
       <ScrollContainerWithTitle
-        className="px-4 md:px-0 my-10 md:mt-4 max-w-screen-2xl mx-auto flex flex-col items-center md:items-start text-center md:text-left"
-        title="Our Latest Event"
+        className="px-4 md:px-0 my-10 md:mt-4 max-w-screen-2xl mx-auto"
+        title={`Our Latest Event`}
         titleClasses="text-primary-700 font-semibold text-[36px]"
+        text={`Venuerific has the experience necessary to manage a variety of venues and to host all kinds of events. From high-profile corporate dinners.`}
+        textClasses="text-black text-[24px] font-medium"
       >
-        <p className="text-black text-[24px] font-medium">
+        {/* <p className="text-black text-[24px] font-medium">
           Venuerific has the experience necessary to manage a variety of venues
           and to host all kinds of events. From high-profile corporate dinners.
-        </p>
+        </p> */}
 
         <HorizontalScrollList items={venueItems} cardType="pvmevents">
           {venueItems.map((item) => (
