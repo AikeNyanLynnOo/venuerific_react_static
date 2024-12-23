4ce35209351df072e01de636ce855bfe087b0f9b
Yoon Myat Sandi <yoonmyatsandi6@gmail.com>
2024-12-23 16:28:22 +0630

    scheduled form, right chat container, state management(needs to improve mobile and tablet views), fix  buttons, remove navbar and footer on mobiles and tablets

--- components/clients/PaymentFormLayout.tsx
+++ components/clients/PaymentFormLayout.tsx
@@ -6,12 +6,13 @@ export default function PaymentFormLayout() {
   return (
     <div>
       <VNFNavbar
-        className="px-4 md:px-5 items-center py-2 fixed"
+        className="px-4 lg:px-5 items-center py-2 fixed lg:block hidden"
         maxWidth="full"
       />
-      <PaymentFormEnquiryDetails/>
 
-      <div className="w-full mt-10 mx-auto px-5 md:px-5 lg:px-12 xl:px-20 block md:flex items-center justify-between">
+      <PaymentFormEnquiryDetails />
+
+      <div className="w-full mt-10 mx-auto px-5 lg:px-12 xl:px-20 hidden lg:block">
         <Footer />
       </div>
     </div>
